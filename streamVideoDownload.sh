1299a9ce0dee12763a7b1f661c2be4d29fd84e89
gejianxin <gejianxin@yahoo.com>
2020-02-03 20:37:13 +0800

    Update streamVideoDownload.sh

--- streamVideoDownload.sh
+++ streamVideoDownload.sh
@@ -3,7 +3,7 @@
 # This script is to download m3u8 format stream video
 # How to run?
 # ./streamVideoDownload.sh [linkfile]
-# Each line in link file should be like "link output"
+# Each line in link file should be like "link [TAB] output"
 # link is the url of the m3u8 file
 # output is the file name of the download file, no extention needed.
 OLDIFS=$IFS
