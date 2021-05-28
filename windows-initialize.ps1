46b4dabe6cc62f3440f5cea7b968ca444d233c29
Ge Jianxin <gejianxin@yahoo.com>
2021-05-28 18:28:34 +0800

    Add powershell initialization script.

---
+++ windows-initialize.ps1
@@ -0,0 +1,68 @@
+# Windows软件部署
+winget install --id Microsoft.PowerToys
+winget install --id Microsoft.Git
+winget install --id Microsoft.Office
+winget install --id Microsoft.WindowsTerminal
+winget install --id Microsoft.VisualStudioCode
+winget install --id Microsoft.PowerBI
+
+# 安装开发工具
+winget install --id Anaconda.Anaconda3
+winget install --id Docker.DockerDesktop
+winget install --id OpenJS.NodeJS
+
+# 安装编辑器
+winget install --id calibre.calibre
+winget install --id GNU.Emacs
+winget install --id SumatraPDF.SumatraPDF
+winget install --id vim.vim
+winget install --id Youdao.YoudaoNote
+winget install --id Zotero.Zotero
+
+# 安装效率工具
+winget install --id Anki.Anki
+winget install --id Bopsoft.Listary
+winget install --id cuiliang.quicker
+winget install --id DominikReichl.KeePass
+winget install --id Ghisler.TotalCommander
+winget install --id Lexikos.AutoHotkey
+winget install --id jely2002.youtube-dl-gui
+winget install --id QL-Win.QuickLook
+winget install --id ShareX.ShareX
+winget install --id voidtools.Everything
+winget install --id XnSoft.XnViewClassic
+
+# 安装常用软件
+winget install --id Tencent.QQ
+winget install --id Tencent.QQPlayer_cn
+winget install --id Tencent.WeChat
+
+# 安装游戏登陆器
+# Playnite可以登录多个平台
+winget install --id Playnite.Playnite
+
+# 安装不常用开发工具
+# winget install --id R
+# winget install --id Python.Python
+# winget install --id Postman.Postman
+# winget install --id PostgreSQL.PostgreSQL
+# winget install --id SBCL.SBCL
+# winget install --id Julialang.Julia
+
+# 安装不常用工具
+# winget install --id Typora.Typora
+# winget install --id Joplin.Joplin
+# winget install --id TUG.TeXLive
+# winget install --id Graphviz.Graphviz
+# winget install --id gnuplot.gnuplot
+# winget install --id GIMP.GIMP
+# winget install --id Inkscape.Inkscape
+# winget install --id ImageMagick.ImageMagick
+# winget install --id OliveTeam.OliveVideoEditor
+# winget install --id FreeTime.FormatFactory
+# winget install --id Rufus.Rufus
+# winget install --id grammarly.GrammarlyForWindows
+# winget install --id grammarly.grammarlyforoffice
+
+# 目前还没有的软件
+# ffmpeg trojan
