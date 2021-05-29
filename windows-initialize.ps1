fd9fcb767a5aa7c52d4526fb02083236fca73e5e
Ge Jianxin <gejianxin@yahoo.com>
2021-05-29 18:13:31 +0800

    Windows initialization powershell script update.

--- windows-initialize.ps1
+++ windows-initialize.ps1
@@ -10,14 +10,34 @@ winget install --id Microsoft.PowerBI
 winget install --id Anaconda.Anaconda3
 winget install --id Docker.DockerDesktop
 winget install --id OpenJS.NodeJS
+winget install --id GnuWin32.Grep
+# winget install --id R
+# winget install --id Python.Python
+# winget install --id Postman.Postman
+# winget install --id PostgreSQL.PostgreSQL
+# winget install --id SBCL.SBCL
+# winget install --id Julialang.Julia
 
 # 安装编辑器
 winget install --id calibre.calibre
 winget install --id GNU.Emacs
-winget install --id SumatraPDF.SumatraPDF
 winget install --id vim.vim
 winget install --id Youdao.YoudaoNote
 winget install --id Zotero.Zotero
+# winget install --id Typora.Typora
+# winget install --id Joplin.Joplin
+# winget install --id Mubu.Mubu
+# winget install --id Notion.Notion
+# winget install --id SumatraPDF.SumatraPDF
+# winget install --id alibaba.yuque
+# winget install --id TUG.TeXLive
+# winget install --id Graphviz.Graphviz
+# winget install --id gnuplot.gnuplot
+# winget install --id GIMP.GIMP
+# winget install --id Inkscape.Inkscape
+# winget install --id ImageMagick.ImageMagick
+# winget install --id OliveTeam.OliveVideoEditor
+# winget install --id FreeTime.FormatFactory
 
 # 安装效率工具
 winget install --id Anki.Anki
@@ -27,42 +47,26 @@ winget install --id DominikReichl.KeePass
 winget install --id Ghisler.TotalCommander
 winget install --id Lexikos.AutoHotkey
 winget install --id jely2002.youtube-dl-gui
+winget install --id OBSProject.OBSStudio
 winget install --id QL-Win.QuickLook
 winget install --id ShareX.ShareX
+winget install --id utools.utools
 winget install --id voidtools.Everything
 winget install --id XnSoft.XnViewClassic
 
 # 安装常用软件
+winget install --id Daum.PotPlayer
 winget install --id Tencent.QQ
-winget install --id Tencent.QQPlayer_cn
 winget install --id Tencent.WeChat
+# winget install --id Tencent.QQPlayer_cn
+# winget install --id nashaofu.dingtalk
+# winget install --id Rufus.Rufus
+# winget install --id grammarly.GrammarlyForWindows
+# winget install --id grammarly.grammarlyforoffice
 
 # 安装游戏登陆器
 # Playnite可以登录多个平台
 winget install --id Playnite.Playnite
-
-# 安装不常用开发工具
-# winget install --id R
-# winget install --id Python.Python
-# winget install --id Postman.Postman
-# winget install --id PostgreSQL.PostgreSQL
-# winget install --id SBCL.SBCL
-# winget install --id Julialang.Julia
-
-# 安装不常用工具
-# winget install --id Typora.Typora
-# winget install --id Joplin.Joplin
-# winget install --id TUG.TeXLive
-# winget install --id Graphviz.Graphviz
-# winget install --id gnuplot.gnuplot
-# winget install --id GIMP.GIMP
-# winget install --id Inkscape.Inkscape
-# winget install --id ImageMagick.ImageMagick
-# winget install --id OliveTeam.OliveVideoEditor
-# winget install --id FreeTime.FormatFactory
-# winget install --id Rufus.Rufus
-# winget install --id grammarly.GrammarlyForWindows
-# winget install --id grammarly.grammarlyforoffice
 
 # 目前还没有的软件
 # ffmpeg trojan
